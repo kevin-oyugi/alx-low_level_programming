6514ab8aa550874a0fcb8ef8d3db58c8c59a8179
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-08-26 13:32:59 -0700

    Puts Function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
-# include <stdio.h>
+#include <stdio.h>
 
-/*print a description */
-
-int main (void)
+/**
+ * main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
