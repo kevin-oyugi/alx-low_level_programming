e82c68b412c8ea842567fb0fa16807f8e6a92d2d
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-08-26 13:26:47 -0700

    prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - print the size of various types
  *
- * Description: prints "Programming is like building a multilingual puzzle"
+ * Description: prints the size of various types the computer it is compiled and
+ * run on it
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
+
 	return (0);
 }
