08b06d08670f7ad1464f6cc65ea3eede7236dfa1
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-08-30 04:37:55 -0700

    Assign a random num to n and print last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,10 +19,11 @@ int main(void)
 
 	last = n % 10;
 	if (last > 5)
-		printf("The last digit is %d and is greater than 5\n", n, last);
+		printf("The last num of %d is %d and is greater than 5\n",
+		       n, last);
 	else if (last < 6 && last !=0)
-		printf("The last digit is %d and is less than 6 and not 0\n", n,
-		       last);
+		printf("The last num of %d is %d and is less than 6 and not 0\n"
+		       , n, last);
 	else if (last == 0)
-		printf("the last digit is %d and is 0\n", n, last);
+		printf("the last num of %d is %d and is 0\n", n, last);
 	return (0);
