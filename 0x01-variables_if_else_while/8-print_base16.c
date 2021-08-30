88d4b93861e1943fc992b11cf3468bb98b9bf6ce
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-08-30 11:16:59 -0700

    prints all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - letters of the alphabet
+ *
+ * Description: print the letters of the alphabet except e, q
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 48)
+	{
+		if (i < 10)
+			putchar(i + '0');
+		else if (i > 41)
+			putchar(i - 10 + 'A');
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
