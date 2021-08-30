5ea36f26c316fdf1caf06111188f5c87dea0f12e
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-08-30 02:28:24 -0700

    Determine if a random num is neg or pos when executed

---
+++ 0x01-variable_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - determine if num is pos, neg or zero
+ *
+ *description: Assign a random num to the varable to be executed
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	if n;
+
+	srand(time(0));
+	n =rand() - RAND_MAX / 2;
+
+	if (n == o)
+		printf("%d is zero\n", n);
+	else if(n > 0)
+		printf("%d is positive\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
+}
---
+++ 0x01-variable_if_else_while/README.md
@@ -0,0 +1,1 @@
+'Loop Statements'
