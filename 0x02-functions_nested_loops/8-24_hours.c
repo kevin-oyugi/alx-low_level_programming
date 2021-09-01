fa46287619c13202424bc8657aacd99a1e25069a
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-09-01 09:55:40 -0700

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hours_1;
+	int hours_2;
+	int minutes_1;
+	int minutes_2;
+	int hours;
+	int minutes;
+
+	for (hours = 0; hours <= 23; hours++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			hours_1 = hours / 10;
+			hours_2 = hours % 10;
+			minutes_1 = minutes / 10;
+			minutes_2 = minuttes % 10;
+
+			_putchar('0' + hours_1);
+			_putchar('0' + hours_2);
+			_putchar(':');
+			_putchar('0' + minutes_1);
+			_putchar('0' + minites_2);
+			_putcahr('\n');
+		}
+	}
+}
