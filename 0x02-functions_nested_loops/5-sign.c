93316cffff37849e9909534ca6523df66cfb2b58
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-09-01 12:06:21 -0700

    prints if a numer is 0,

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *main: print_sign - print sign
+ * main: print_sign - print sign
  *
- *Definition:
+ * Description:
  *
- *@n: Interger
+ * @n: Interger
  *
- *Return: 1 if  +, 0 if 0, -1 if -
+ * Return: 1 if  +, 0 if 0, -1 if -
  */
 int print_sign(int n)
 {
@@ -16,14 +16,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar('0');
-		return (0);
-	}
-	else (n < 0)
-        {
 		_putchar('-');
-	        return (-1);
+		return (-1);
+	}
+	else
+        {
+		_putchar('0');
+	        return (0);
         }
 }
