6da2fbd95b583681d0b71d6099d845d447626d64
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-09-01 04:48:52 -0700

    print both upper and lower case letteres

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *main: _isalpha - checks for lowercase and uppercase
+ *
+ *Definition: upercase and lowercase letter
+ *
+ *@c: Ascii character
+ *
+ *Return: 1 (if uppercase or lowercase)
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return = (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return = (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
