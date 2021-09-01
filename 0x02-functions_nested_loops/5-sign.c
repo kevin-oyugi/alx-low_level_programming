3eeedb7e6c0f9279a0dc64f72a9593bf35175c95
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-09-01 05:14:13 -0700

    positive, negative or zero

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *main: print_sign - print sign
+ *
+ *Definition:
+ *
+ *@n: Interger
+ *
+ *Return: 1 if  +, 0 if 0, -1 if -
+ */
+int print_sign(int n)
+{
+	if (n >= 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else (n < 0)
+	     {
+		     _putchar('-');
+		     return (-1);
+	     }
+}
