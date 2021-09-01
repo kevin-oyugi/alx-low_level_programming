857cafd77edfe8256f0bf57ab1a086de3ef728e8
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-09-01 06:07:09 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *main: print_last_digit - last digit
+ *
+ *Definition: print last digit
+ *
+ *@n: interger
+ *
+ *Return: Interger
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+	last = n % 10;
+
+	if (n < 0)
+		last = * -1;
+	_putchar(last + '0');
+
+	return last;
+}
