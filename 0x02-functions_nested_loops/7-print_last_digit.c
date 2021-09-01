75bf15ab6f59f4df4b3f676e1086ec6c2eb7e060
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-09-01 06:20:55 -0700

    print last digit of a num

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,11 +12,10 @@
 
 int print_last_digit(int n)
 {
-	int last;
-	last = (n % 10);
-
+	int last = n % 10;
+	
 	if (n < 0)
-		last = * -1;
+		last = last * -1;
 	_putchar(last + '0');
 
 	return last;
