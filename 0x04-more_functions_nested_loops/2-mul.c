42cf28d8fd0528ddf79f450bae38d7bbe34a6d51
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-09-03 02:35:41 -0700

    multiply two interger

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiply a and b
+ * @a: input a
+ * @b: input b
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
