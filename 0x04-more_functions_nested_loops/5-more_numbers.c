1a3affcbd17bf01d613c3dc823722589a1f1cf81
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-09-03 04:07:25 -0700

    print 0 - 14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers 1 to 14
+ * Return: no return
+ *
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	int x = 0;
+	int u1 = 0;
+	int u2 = 0;
+
+	for (x = 1; x <= 10; x++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				u1 = i / 10;
+				_putchar('0' + u1);
+			}
+			u2 = i % 10;
+			_putchar('0' + u2);
+		}
+		_putchar('\n');
+	}
+}
