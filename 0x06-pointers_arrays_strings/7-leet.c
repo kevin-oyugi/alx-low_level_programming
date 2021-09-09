dc02e3eb00d4aff3cebc518ca780993cdba698e9
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-09-09 13:13:11 -0700

    encode a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main: leet - put 1 when is a uppercase
+ * Definition:
+ * @s: takes in an integer
+ * Return: 1 if is uppercase and 0 if is lowcase
+ */
+
+char *leet(char *s)
+{
+	int len;
+	int i;
+	char let[10] = "aAeEoOtTlL";
+	char num[10] = "4433007711";
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+		for (i = 0; let[i] != '\0'; i++)
+		{
+			if (s[len] == let[i])
+			{
+				s[len] = num[i];
+			}
+		}
+	}
+	return (s);
+}
