531980ae026c30bafe68198c33ee52f7008faf60
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-09-09 12:46:53 -0700

    changes all lowercase letters of a string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main: string_toupper - put 1 when is a uppercase
+ * Definition:
+ * @s: takes in an integer
+ * Return: 1 if is uppercase and 0 if is lowcase
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
