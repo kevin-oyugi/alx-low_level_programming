0565bac204c644c9524972f7122be837e169281d
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-09-09 09:49:50 -0700

    Concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main: _strcat - concatenates two words.
+ * Definition:
+ * @dest: output
+ * @src: input
+ * Return: x.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *x = dest;
+
+	for (; *dest != '\0' ; dest++)
+	{
+		;
+	}
+	for (; *src != '\0'; src++)
+	{
+		*dest = *src;
+		dest++;
+	}
+	*dest = '\0';
+	return (x);
+}
