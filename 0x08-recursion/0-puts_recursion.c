5c9aa8d05d9464f8895920ce69222d941d3e9975
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-09-16 11:09:38 -0700

    a function that prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main:_print_rev_recursion - prints a string in reverse
+ * main: _print_rev_recursion - prints a string in reverse
  * Definition:
  * @s: string to print
  * Return: Nothing
