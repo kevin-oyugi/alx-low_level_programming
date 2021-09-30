d14e9e3c1554ff03038b5b4c5eea18d581b0f211
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-09-30 12:50:42 -0700

    a function that executes a function given as a parameter on each element of an array

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,8 +2,12 @@
 #define POINTERS_FUNTIONS
 
 #include <stdlib.h>
+
 #include <stddef.h>
+
 int _putchar(char c);
+
 void print_name(char *name, void (*f)(char *));
 
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
