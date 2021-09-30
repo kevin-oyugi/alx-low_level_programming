c5038e95600a9a22d49928d6fd46ff9fb2efb081
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-09-30 13:29:17 -0700

    function that prints strings, followed by a new line

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -22,5 +22,6 @@ typedef struct printer
 int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
-#endif
 
+void print_strings(const char *separator, const unsigned int n, ...);
+#endif
