22443a9496b25703bccfefa7ee0bc82ecdcc65ae
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-10-08 04:12:56 -0700

    function that prints all the elements of a list_t list

---
+++ 0x12-singly_linked_lists/main.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for ALX  project
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+#endif
